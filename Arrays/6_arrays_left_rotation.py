cb3e6ed2b5866522b2d171440f72e5ef37660cab
Sarah Fulkerson <contact@sarahfulkerson.com>
2020-10-27 21:33:12 -0500

    additional excercises
    exercise 6 is passing all test cases but exercise 7 still needs some work

--- Arrays/6_arrays_left_rotation.py
+++ Arrays/6_arrays_left_rotation.py
@@ -6,14 +6,19 @@ def rotLeft(a, d):
     a = an array of integers
     d = an integer; the number of rotations to perform on 'a'
     """
-    #TODO: FIX OOM ERRORS
     res = a
-    for x in range(d):
-        res = res[1:] + [res[0]]
+
+    if len(res) // d != 0:
+        res = res[d:] + res[:d]
+    else:
+        shift = d % len(res)
+        res = res[shift:] + res[:shift]
     
     return res
 
 if __name__ == '__main__':
+    a = [10, 20, 30, 40, 50]
+    d = 4
     a = [10, 20, 30]
-    d = 7
+    d = 8
     print(rotLeft(a,d))
